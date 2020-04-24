67621bfd22341d42754742893e0d75fb55695640
Michael Le <mnle529@gmail.com>
2020-04-23 21:59:01 -0700

    changes to image links

--- src/components/Headshot/Headshot.js
+++ src/components/Headshot/Headshot.js
@@ -4,7 +4,7 @@ function Headshot() {
     // console.log(props.photo);
     return (
         <div className="image-shadow">
-            <img src="/react-portfolio/assets/images/headshot.jpg" alt="Headshot" className="img-responsive" />
+            <img src="/assets/images/headshot.jpg" alt="Headshot" className="img-responsive" />
         </div>
     )
 }
