5010a356243b481ad38645225fbbaebec38f73b5
Michael Le <mnle529@gmail.com>
2020-04-24 03:12:51 -0700

    fixing bugs

--- src/data/about-data.js
+++ src/data/about-data.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     bio: "I am a full stack software developer with a passion for finding solutions to complex problems and the ability to see projects through from concept to completion.  I'm comfortable throughout the stack, but my strength lies in the front end, at the intersection of code and design.  I am proficient in HTML, CSS, Bootstrap, JavaScript, jQuery, and Node.js, and am learning more every day.  Check out my work, and let me know if I can help you too!",
-    photo: "react-portfolio/assets/images/headshot.jpg",
+    photo: "/react-portfolio/assets/images/headshot.jpg",
     resume: ""
 };
