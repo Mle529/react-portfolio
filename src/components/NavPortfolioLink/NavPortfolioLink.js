09252ead95a7d416df19fd43faecf67ca7a1a5fb
Michael Le <mnle529@gmail.com>
2020-04-25 01:05:04 -0700

    trying hashbrowser

--- src/components/NavPortfolioLink/NavPortfolioLink.js
+++ src/components/NavPortfolioLink/NavPortfolioLink.js
@@ -4,7 +4,7 @@ function NavPortfolioLink(props) {
 
     return (
         <li id="portfolio-item" className={(props.active === "true") ? "nav-item active" : "nav-item"}>
-            <a id="portfolio-link" className="nav-link" href="/react-portfolio/portfolio">{(props.active === "true") ? <>Portfolio<span className='sr-only'>(current)</span></> : <>Portfolio</>}</a>
+            <a id="portfolio-link" className="nav-link" href="/#/portfolio">{(props.active === "true") ? <>Portfolio<span className='sr-only'>(current)</span></> : <>Portfolio</>}</a>
         </li>
     );
 }
